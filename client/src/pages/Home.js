edb0fdadc00ab6c4f409a3594ea8f5361f659344
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-03-07 05:45:43 +0100

    imported Header, Services , about, reason and footer components

---
+++ client/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import AboutUs from '../components/AboutUs'
+import Header from '../components/Header'
+import Reason from '../components/Reason'
+import Services from '../components/Services'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AboutUs />
+        <Services />
+        <Reason />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
