7dbcb91b94d9e1e5cd9ff7242353f2eef109e8f3
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-03-24 01:50:43 +0100

    Added a gsap animated read more or less component

---
+++ client/src/components/gsap/ShowMore.js
@@ -0,0 +1,51 @@
+import React, { useState, useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+
+const ShowContent = ({ children, visible, vars }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  let textRef = useRef(null);
+  const tl = useRef(null)
+
+  useLayoutEffect(() => {
+
+    tl.current = gsap.timeline({ paused: true, reversed: true })
+    tl.current.from(textRef, {
+      height: 0,
+      width: 0,
+      autoAlpha: 0,
+      ease: 'power2.in'
+    })
+    //.from(container, { height: height, ...vars, ease: 'power2.in' }, '-=0.5')
+
+    //console.log(tl.current.reversed())
+
+    return () => tl.current.progress(0).kill
+
+  }, [])
+
+  const handleShowMore = () => {
+    tl.current.reversed()
+      ? tl.current.play() 
+      : tl.current.reverse();
+
+    setShowMore(prev => !prev)
+  }
+
+  return (
+    <div>
+      <p className="main-text card-text">
+        <span>{visible}</span>
+        <span 
+          className={`${showMore ? "" : "hidden"}`}  
+          ref={ el => textRef = el}
+        >{children}</span>
+        <button className="main-text show-btn" onClick={handleShowMore}>
+          ...{`${!showMore ? "Read more" :  "Show less"}`}
+        </button>
+      </p>
+    </div>
+  )
+}
+
+export default ShowContent;
