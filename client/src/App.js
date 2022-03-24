863bada9b906ba1f23999a313fa08201d0d1e0a5
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-03-24 02:00:38 +0100

    Imported the scrollTop and single member component

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,19 @@ import {Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import OurServices from './pages/OurServices';
+import SingleMember from './pages/SingleMember';
+import ScrollTop from './components/ScrollTop';
 
 function App() {
   return (
     <ApolloProvider client={client}>
+      <ScrollTop />
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="services" element={<OurServices />} />
+          <Route path="team-member/:nameid" element={<SingleMember />} / >  
         </Route>
       </Routes>
     </ApolloProvider>
