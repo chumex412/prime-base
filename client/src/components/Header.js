0005f5e7ad0cf0894cee9653baac7705f6eb1787
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-03-24 01:47:02 +0100

    Added animation to the header and hero section

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,46 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef, useState, useLayoutEffect} from 'react';
+import gsap from 'gsap';
 import Nav from "./Nav";
 import Hero from './Hero';
 import { useLocation } from 'react-router-dom';
+import CSSRulePlugin from 'gsap/CSSRulePlugin';
 import '../styles/header.css';
+import heroHome from '../assets/Hero-img.png';
+import otherHero from '../assets/hero-2.png';
 
-function Header() {
+function Header({ pageInfo }) {
   const [height, setHeight] = useState("");
   const {pathname} = useLocation();
 
+  gsap.registerPlugin(CSSRulePlugin);
+
   const headerRef = useRef(null);
+  
 
   useEffect(() => {
     if(pathname === "/") {
-      setHeight("home-height")
+      setHeight("home-height");
+      headerRef.current.style.backgroundImage =  `url(${heroHome})`;
     } else {
-      setHeight("others-height")
+      setHeight("others-height");
+      headerRef.current.style.backgroundImage =  `url(${otherHero})`;
     }
   }, [pathname]);
 
+  useLayoutEffect(() => {
+    const rule = CSSRulePlugin.getRule(`.header::before`)
+    gsap.fromTo(rule, { 
+      cssRule: {width: 0, backgroundColor: 'rgba(25, 28, 35, 0.2)'} 
+    }, { 
+      cssRule: {width: "100%", backgroundColor: 'rgba(25, 28, 35, 0.9)' 
+    }, duration: 1, ease: 'power2.in' })
+      
+  })
+
   return (
-    <header className={`header ${height}`} ref>
+    <header className={`header ${height}`} ref={headerRef}>
       <Nav />
-      <Hero />
+      <Hero {...pageInfo} />
     </header>
   )
 }
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,18 +1,95 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import '../styles/hero.css';
+import { textPopUp } from './gsap/textAnimation';
 
-function Hero() {
-  return (
-    <section className="hero">
-      <div className="container">
-        <div className="hero-center">
-          <h1 className="h1">Establishing property boundaries to ensure safe and fit infrastructures</h1>
-          <p className="intro-text">Our deep Land survey experience and knowledge of civil engineering  ensures inspection of your property produces detailed and accurate data critical to its structural integrity.</p>
-          <button type="button" className="btn hero-btn">Contact us</button>
+function Hero({title, page1, page2}) {
+  const { pathname } = useLocation();
+  let heroRef = useRef(null);
+  //let otherPathRef = useRef(null);
+  let otherContent = useRef(null); 
+  //const elems = gsap.utils.selector(heroRef);
+  //const tl = useRef(null);
+
+  useLayoutEffect(() => {
+    if(pathname === "/") {
+      const heroContents = heroRef.children;
+      const tween = textPopUp(heroContents, { scale: 0, opacity: 0, delay: 0.5 });
+      
+      return () => tween.kill()
+    } else {
+
+      const title = otherContent.querySelector('h1');
+      const breadcrumb = otherContent.querySelector('.breadcrumb');
+      const tween1 = textPopUp(title, { scale: 0, delay: 0.5 });
+      const tween2 = gsap.from(breadcrumb.children, { 
+        x: -500, autoAlpha: 0, duration: 1, ease: 'power4.out', delay: 0.5, stagger: { 
+          amount: 1, 
+          from: 'end' 
+        } 
+      })
+      
+      return () => {
+        tween1.kill();
+        tween2.kill();
+      }
+    }
+  }, [])
+
+  if(pathname === "/") {
+    return (
+      <section className="hero">
+        <div className="container">
+          <div className="hero-center" ref={el => heroRef = el}>
+            <h1 className="h1 hero-title">Establishing property boundaries to ensure safe and fit infrastructures</h1>
+            <p className="intro-text hero-text">Our deep Land survey experience and knowledge of civil engineering  ensures inspection  of your property produces detailed and accurate data critical to its structural integrity.</p>
+            <button type="button" className="btn hero-btn">Contact us</button>
+          </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
+
+  if(pathname !== "/" && page1.length) {
+    const newPage1 = page1.split(" ")[0];
+    return (
+      <section className="hero">
+        <div className="container">
+          <div className="hero-center" ref={el => otherContent = el}>
+            <h1 className="h1">{title}</h1>
+            <strong className="breadcrumb">
+              <NavLink 
+                to="/" 
+                className={({ isActive }) => `h4 breadcrumb-link ${isActive ? "active" : ''}`}
+              >Home</NavLink>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="18" height="18" viewBox="0 0 18 18">
+                <path fill="#fff" d="M6.8 15.5a.9.9 0 0 1-.8-.6.8.8 0 0 1 .2-.9l5-5-5-5a.8.8 0 1 1 1.1-1.2L13 8.4a.8.8 0 0 1 0 1.2l-5.7 5.6c-.1.2-.3.3-.5.3Z"/>
+              </svg>
+              <NavLink 
+                to={`/${newPage1}`} 
+                className={({ isActive }) => `h4 breadcrumb-link ${isActive ? "active" : ''}`}
+              >{page1}</NavLink>
+              {
+                page2.text && (
+                  <>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="18" height="18" viewBox="0 0 18 18">
+                      <path fill="#fff" d="M6.8 15.5a.9.9 0 0 1-.8-.6.8.8 0 0 1 .2-.9l5-5-5-5a.8.8 0 1 1 1.1-1.2L13 8.4a.8.8 0 0 1 0 1.2l-5.7 5.6c-.1.2-.3.3-.5.3Z"/>
+                    </svg>
+                    <NavLink 
+                      to={`/${page2.text.split(" ").join("-")}/${page2.path}`}
+                      className={ ({ isActive }) => `h4 breadcrumb-link ${isActive ? "active" : ""}` }
+                    >{page2.text}</NavLink>
+                  </>
+                )
+              }
+            </strong>
+          </div>
+        </div>  
+      </section>
+    )
+  }
 }
 
 export default Hero
