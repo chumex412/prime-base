31be303e4b6e5ff0c9cf4a0fce93944a8d224b87
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-03-24 02:03:12 +0100

    imported components for the about us page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import AboutUs from '../components/AboutUs';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
+import OurTeam from '../components/OurTeam';
 
 function About() {
   return (
     <>
-      <Header />
+      <Header pageInfo={{title: "who we are", page1: "about us", page2: ""}} />
+      <main>
+        <AboutUs />
+        <OurTeam />
+      </main>
+      <Footer />
     </>
   )
 }
