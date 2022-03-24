f28695abe6990f8712820d909d41c897bd8330d1
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-03-24 01:54:58 +0100

    Added components for the reasons section

--- client/src/components/Reason.js
+++ client/src/components/Reason.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
 import '../styles/reasons.css';
 import ReasonList from './ReasonList';
+import{ fadeInTl, fadeOut } from './gsap/fadeAnimation';
+import { slideInTl } from './gsap/slideAnimation';
+import useIntersection from '../custom hook/useIntersection';
 
 function Reason() {
+
+  const headerRef = useRef(null);
+
+  const childElem = gsap.utils.selector(headerRef);
+
+  let masterTl = useRef(null)
+
+  const [ref, isVisible] = useIntersection(headerRef, 0.3)
+
+  useLayoutEffect(() => {
+    const span1 = childElem('.h3>span:first-child');
+
+    const span2 = childElem('.h3>span:last-child');
+
+    masterTl.current = gsap.timeline({ paused: true })
+
+    masterTl.current.add(slideInTl(span1, { autoAlpha: 1, duration: 1 }), 1)
+      .add(slideInTl(span2, { autoAlpha: 1, duration: 1 }, 'back.out'), 0)
+      .add(fadeInTl(childElem('.intro-text'), { duration: 0.5 }), '-=1')
+
+    if(isVisible) {
+      masterTl.current.play();
+    } else {
+      fadeOut(span1, { x: -400 })
+      fadeOut(span2, { x: -400 })
+      fadeOut(childElem('.intro-text'), { y: 60 })
+    }
+
+    return () => masterTl.current.progress(0).kill();
+    
+  }, [isVisible, ref, childElem])
+
   return (
     <section className="reason">
       <div className="container">
-        <section className="reasons-header">
+        <section className="reasons-header" ref={headerRef}>
           <h2 className="h3">
             <span>Why choose</span>
               {' '}
--- client/src/components/ReasonList.js
+++ client/src/components/ReasonList.js
@@ -1,22 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
 import { reasonList } from '../data/data';
+import useIntersection from '../custom hook/useIntersection';
+import { fadeIn, fadeOut } from './gsap/fadeAnimation';
 
 function ReasonList() {
   const [reasons, setReasons] = useState({choose_us: []});
 
+  const listRef = useRef(null);
+  
+  const [ref, isVisible] = useIntersection(listRef, 0.3);
+
   useEffect(() => {
     setReasons(reasonList);
-  }, [])
+  }, []);
+
+  useLayoutEffect(() => {
+    const elem = ref.current;
+
+    if(isVisible) {
+      fadeIn(elem, { y: 0, paused: true, duration: 1.5 }).play();
+    } else {
+      fadeOut(elem, { y: 100 });
+    }
+
+  }, [isVisible, ref]);
 
   const { choose_us } = reasons;
   return (
-    <section className="reason-list row">
+    <section className="reason-list row" ref={listRef}>
       {
         choose_us.map(reason => {
           const {id, title, text, icon} = reason;
 
           return (
-            <div  key={id} className="col-sm-12 col-md-4">
+            <div  key={id} className="col-xs-12 col-sm-6 col-md-4">
               <article>
                 <div className="reason-icon">{icon}</div>
                 <h3 className="h4">{title}</h3>
