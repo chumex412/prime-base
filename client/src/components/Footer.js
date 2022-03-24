442e2bbcf5d5c8a2d432bdce65c5ee2fe4fb4fbf
chumex412 <64347090+chumex412@users.noreply.github.com>
2022-03-24 01:46:06 +0100

    Footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
 import Mainlogo from './Mainlogo';
 import { Link } from 'react-router-dom';
 import '../styles/footer.css';
 import { contactList } from '../data/data';
+import useIntersection from '../custom hook/useIntersection';
+import { fadeIn, fadeInTl, fadeOut } from './gsap/fadeAnimation';
 
 function Footer() {
+
+  const contentRef = useRef(null);
+  let linksRef = useRef(null);
+  let contactRef = useRef(null)
+  const masterTl = useRef(null);
+
+  const [ref, isVisible] = useIntersection(contentRef);
+  const childElem = gsap.utils.selector(ref)
+
+  useLayoutEffect(() => {
+    masterTl.current = gsap.timeline({ paused: true })
+
+    masterTl.current.add(fadeIn(childElem('.logo'), { scale: 1, duration: 1.5 }, 'bounce'))
+      .add(fadeIn(childElem('.footer-sub_nav h2'), { scale: 1, duration: 1.5 }, 'bounce'), '<')
+      .add(fadeIn(childElem('.footer-contact h2'), { scale: 1, duration: 1.5 }, 'bounce'), '<')
+
+    if(isVisible) {
+      masterTl.current.play()
+      fadeIn(linksRef.children, { stagger: 0.5, duration: 1})
+      fadeIn(contactRef.children, { stagger: 0.5, duration: 1})
+    } else {
+      fadeOut(childElem('.logo'), { scale: 0 })
+      fadeOut(linksRef.children, { stagger: 0.1, y: 100 })
+      fadeOut(contactRef.children, { stagger: 0.1, y: 100 })
+      fadeOut(childElem('.footer-sub_nav h2'),  { scale: 0 })
+      fadeOut(childElem('.footer-contact h2'),  { scale: 0 })
+    }
+
+    return () => {
+      masterTl.current.progress(0).kill();
+    }
+
+  }, [isVisible, childElem])
+
   return (
     <footer className="footer">
       <div className="footer-top">
         <div className="container">
-          <div className="footer-center">
+          <div className="footer-center" ref={contentRef}>
             <div className="footer-brand">
               <Mainlogo />
             </div>
 
             <div className="footer-sub_nav">
               <h2>Quick Link</h2>
-              <ul className="footer-list">
+              <ul className="footer-list" ref={el => linksRef = el}>
                 <li className="footer-item">
                   <Link to="/" className="footer-link intro-text">Home</Link>
                 </li>
@@ -30,7 +67,7 @@ function Footer() {
             </div>
             <div className="footer-contact">
               <h2 >Office Address</h2>
-              <ul className="footer-list">
+              <ul className="footer-list" ref={el => contactRef = el}>
                 {
                   contactList.map(item => {
                     const { id, name, text, icon } = item;
@@ -54,11 +91,13 @@ function Footer() {
         </div>
       </div>
       <div className="footer-bottom">
-        <p className="main-text">
-          <span>Copyright &copy;</span>{" "}
-          <span>{new Date().getFullYear()}</span>{" "}
-          <span>Prime Base Geo-Services. All rights reserved.</span>
-        </p>
+        <div className="container">
+          <p className="main-text">
+            <span>Copyright &copy;</span>{" "}
+            <span>{new Date().getFullYear()}</span>{" "}
+            <span>Prime Base Geo-Services. All rights reserved.</span>
+          </p>
+        </div>
       </div>
     </footer>
   )
